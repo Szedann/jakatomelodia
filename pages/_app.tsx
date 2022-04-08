f4bff15b58dac2de2e3383fcbc59a42cc29d8f09
Szedann <65607498+Szedann@users.noreply.github.com>
2022-04-08 10:30:33 +0200

    make money

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1184252809909531"
+     crossorigin="anonymous"></script>
+      <Component {...pageProps} />
+    </>
 }
 
 export default MyApp
