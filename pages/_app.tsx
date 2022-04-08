4b88f255317c3d4aef8f7ea5849b86d3a7d303ef
Szedann <kapslok1211@gmail.com>
2022-04-08 17:48:13 +0200

    unmake money

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1184252809909531"
-     crossorigin="anonymous"></script>
+      {/* <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1184252809909531" crossOrigin="anonymous"></script>
+      </Head> */}
       <Component {...pageProps} />
     </>
 }
